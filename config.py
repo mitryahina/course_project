c767bf118b8ee777b864826c72b4d496ac14d39d
Yevhenia Mitryahina <mitryahina@ucu.edu.ua>
2018-05-12 23:13:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 from newsapi import NewsApiClient
 
 # Initializing
-newsapi = NewsApiClient(api_key='ce605f777cfe45f7883bdebddc674fed')
+newsapi = NewsApiClient(api_key='x')
 
 import tweepy
 
-consumer_key = "oCP8hMSYpC0jZ8mYIoRjvdzic"
-consumer_secret = "FoVR1k8OsoV1ygaB50cKawSFHOwVA1w2Wi2wKFsJkuAoQ7JNFH"
-access_key = "963664485755031553-Oj2bWzLidS9VHCdzPSKrfInVfudpaRa"
-access_secret = "ZqmIJVRoohgK2aI34OHggcs08OzQysW6s9Esz8wUXZ9SH"
+consumer_key = "x"
+consumer_secret = "x"
+access_key = "x"
+access_secret = "x"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
