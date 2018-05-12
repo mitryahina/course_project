4dc6cbfe370c9f5a556708a42b3f1eef911c3382
Yevhenia Mitryahina <mitryahina@ucu.edu.ua>
2018-05-12 23:40:53 +0300

    Update trend.py

--- trend.py
+++ trend.py
@@ -1,4 +1,7 @@
 def get_trends(path):
+    """
+    Read file with trends
+    """
     with open(path, 'r', encoding='UTF-8') as f:
         data = f.read()[14:-4]
         trends = data.split('}, {')
@@ -6,6 +9,9 @@ def get_trends(path):
 
 
 def parse(trend):
+    """
+    Form a dictionary of trend's attributes
+    """
     splitted = trend.split(",")
     res = {}
     for i in range(len(splitted)):
@@ -14,6 +20,9 @@ def parse(trend):
 
 
 class Trend:
+    """
+    Represents Trend with its name, url and volume
+    """
     def __init__(self, trend_dict):
         self.name = trend_dict.get("'name'")[1:-1]
         self.url = trend_dict.get(" 'url'")[1:-1]
@@ -21,6 +30,9 @@ class Trend:
 
 
 class DayTrends:
+    """
+    Contains trends for a given day
+    """
     DAY_TRENDS = []
 
     def __init__(self, lst, date):
@@ -34,7 +46,3 @@ class DayTrends:
             res += 'name: {}\nurl: {}\nvolume: {}\n\n'\
                 .format(i.name, i.url, i.volume)
         return res
-
-
-trends = get_trends('data/trends_5_8_2018.txt')
-print(DayTrends(trends, ('5/8/2018')))
