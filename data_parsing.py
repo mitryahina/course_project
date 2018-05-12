791258f0b9e68dad80c1c65c655d308071baf67b
Yevhenia Mitryahina <mitryahina@ucu.edu.ua>
2018-05-12 23:29:48 +0300

    Update data_parsing.py

--- data_parsing.py
+++ data_parsing.py
@@ -1,7 +1,12 @@
+# Module to parse data from newapi and Twitter API
 from config import newsapi, twitter_api
 
 
 def get_trends_names(location = 23424976):
+    """
+    Function to parse trends dictionary. Returns trends names for the
+    given location
+    """
     trends = twitter_api.trends_place(location)[0].get('trends')
     name_trends = []
     for i in trends:
@@ -10,6 +15,10 @@ def get_trends_names(location = 23424976):
 
 
 def get_news_headlines():
+    """
+    Function to parse news dictionary. Returns news titles for the
+    given location
+    """
     news = newsapi.get_top_headlines(country='ua').get('articles')
     titles = []
     for i in news:
